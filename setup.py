2a9a6b431448c17622eeafe614a4607622e65a10
Philip Hartout <philip.hartout@protonmail.com>
2024-11-13 16:01:48 +0100

    add updated build system

--- setup.py
+++ setup.py
@@ -16,25 +16,12 @@ class CMakeBuild(build_ext):
                     "-std=c++11",
                     "-o",
                     "graph_gen_gym/orca",
-                    "orca_src/orca.cpp",
+                    "graph_gen_gym/orca_src/orca.cpp",
                 ]
             )
         build_ext.run(self)
 
 
 setup(
-    name="graph_gen_gym",
-    version="0.1",
-    packages=["graph_gen_gym"],
     cmdclass={"build_ext": CMakeBuild},
-    install_requires=[
-        "numpy",
-        "torch",
-        "torch_geometric",
-        "scipy",
-        "pydantic",
-        "networkx",
-        "joblib",
-        "appdirs",
-    ],  # Add your Python dependencies here
 )
