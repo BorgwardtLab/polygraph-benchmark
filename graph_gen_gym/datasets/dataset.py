c99cd5d27481ab987adf108ebe689c78f190c08a
Philip Hartout <philip.hartout@protonmail.com>
2024-11-15 13:00:33 +0100

    remove abtrasct_dataset and move definitions to dataset

--- graph_gen_gym/datasets/dataset.py
+++ graph_gen_gym/datasets/dataset.py
@@ -3,10 +3,46 @@
 Implementation of datasets.
 """
 
+from abc import ABC, abstractmethod
 from typing import List, Union
 
-from graph_gen_gym.datasets.abstract_dataset import AbstractDataset
+import networkx as nx
+from torch_geometric.data import Data
+from torch_geometric.utils import to_networkx
+
 from graph_gen_gym.datasets.graph import Graph
+
+
+class AbstractDataset(ABC):
+    @property
+    def identifier(self) -> str:
+        return f"{self.__module__}.{self.__class__.__qualname__}"
+
+    def to_nx(self) -> "NetworkXView":
+        return NetworkXView(self)
+
+    def is_valid(self, graph: nx.Graph) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, idx: int) -> Data:
+        ...
+
+    @abstractmethod
+    def __len__(self) -> int:
+        ...
+
+
+class NetworkXView:
+    def __init__(self, base_dataset: AbstractDataset):
+        self._base_dataset = base_dataset
+
+    def __len__(self):
+        return len(self._base_dataset)
+
+    def __getitem__(self, idx: int) -> nx.Graph:
+        pyg_graph = self._base_dataset[idx]
+        return to_networkx(pyg_graph, to_undirected=True)
 
 
 class GraphDataset(AbstractDataset):
