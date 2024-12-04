6ef4f9ae184d4023efce73b12f212c8ba6777077
Markus Krimmel <krimmel@biochem.mpg.de>
2024-12-04 14:53:41 +0100

    Adding random GIN graph descriptor (#12)
    * Adding random GIN graph descriptor (WIP)
    
    * Added NormalizedDescriptor

--- graph_gen_gym/metrics/utils/graph_descriptors.py
+++ graph_gen_gym/metrics/utils/graph_descriptors.py
@@ -2,13 +2,17 @@ import os
 import subprocess
 import tempfile
 from pathlib import Path
-from typing import Iterable
+from typing import Callable, Iterable
 
+import dgl
 import networkx as nx
 import numpy as np
+import torch
 from scipy.sparse import csr_array
+from sklearn.preprocessing import StandardScaler
 
 import graph_gen_gym
+from graph_gen_gym.metrics.utils.gin import GIN
 
 
 def _edge_list_reindexed(graph: nx.Graph):
@@ -121,3 +125,82 @@ class EigenvalueHistogram:
             spectral_pmf = spectral_pmf / spectral_pmf.sum()
             histograms.append(spectral_pmf)
         return np.stack(histograms, axis=0)
+
+
+class RandomGIN:
+    def __init__(
+        self,
+        num_layers: int = 3,
+        hidden_dim: int = 35,
+        neighbor_pooling_type: str = "sum",
+        graph_pooling_type: str = "sum",
+        input_dim: int = 1,
+        edge_feat_dim: int = 0,
+        dont_concat: bool = False,
+        num_mlp_layers: int = 2,
+        output_dim: int = 1,
+        node_feat_loc: str = "attr",
+        edge_feat_loc: str = "attr",
+        init: str = "orthogonal",
+        device: str = "cpu",
+    ):
+        self.model = GIN(
+            num_layers=num_layers,
+            hidden_dim=hidden_dim,
+            neighbor_pooling_type=neighbor_pooling_type,
+            graph_pooling_type=graph_pooling_type,
+            input_dim=input_dim,
+            edge_feat_dim=edge_feat_dim,
+            num_mlp_layers=num_mlp_layers,
+            output_dim=output_dim,
+            init=init,
+        )
+
+        self.model.node_feat_loc = node_feat_loc
+        self.model.edge_feat_loc = edge_feat_loc
+
+        self.model.eval()
+
+        if dont_concat:
+            self.model.forward = self.model.get_graph_embed_no_cat
+        else:
+            self.model.forward = self.model.get_graph_embed
+
+        self.model.device = device
+        self.model = self.model.to(device)
+
+    @torch.inference_mode()
+    def __call__(self, graphs: Iterable[nx.Graph]):
+        node_feat_loc = self.model.node_feat_loc
+        edge_feat_loc = self.model.edge_feat_loc
+
+        dgl_graphs = [dgl.from_networkx(g) for g in graphs]
+
+        ndata = [node_feat_loc] if node_feat_loc in dgl_graphs[0].ndata else "__ALL__"
+        edata = [edge_feat_loc] if edge_feat_loc in dgl_graphs[0].edata else "__ALL__"
+        graphs = dgl.batch(dgl_graphs, ndata=ndata, edata=edata).to(self.model.device)
+
+        if node_feat_loc not in graphs.ndata:  # Use degree as features
+            feats = graphs.in_degrees() + graphs.out_degrees()
+            feats = feats.unsqueeze(1).type(torch.float32)
+        else:
+            feats = graphs.ndata[node_feat_loc]
+        feats = feats.to(self.model.device)
+
+        graph_embeds = self.model(graphs, feats)
+        return graph_embeds.cpu().detach().numpy()
+
+
+class NormalizedDescriptor:
+    def __init__(
+        self,
+        descriptor_fn: Callable[[Iterable[nx.Graph]], np.ndarray],
+        ref_graphs: Iterable[nx.Graph],
+    ):
+        self._descriptor_fn = descriptor_fn
+        self._scaler = StandardScaler()
+        self._scaler.fit(self._descriptor_fn(ref_graphs))
+
+    def __call__(self, graphs: Iterable[nx.Graph]):
+        result = self._descriptor_fn(graphs)
+        return self._scaler.transform(result)
