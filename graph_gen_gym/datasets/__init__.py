a2270372ddb8caf2dfa4ed92645b89018a221afb
Philip Hartout <philip.hartout@protonmail.com>
2024-12-21 00:21:54 +0100

    fix inits

--- graph_gen_gym/datasets/__init__.py
+++ graph_gen_gym/datasets/__init__.py
@@ -1,5 +1,19 @@
-from .ego import *  # noqa
-from .lobster import *  # noqa
-from .proteins import *  # noqa
-from .spectre import *  # noqa
-from .qm9 import *  # noqa
+from .qm9 import QM9
+from .base.dataset import GraphDataset, OnlineGraphDataset, AbstractDataset
+from .ego import EgoGraphDataset, SmallEgoGraphDataset
+from .lobster import LobsterGraphDataset
+from .spectre import PlanarGraphDataset, SBMGraphDataset
+from .proteins import DobsonDoigGraphDataset
+
+__all__ = [
+    'AbstractDataset',
+    'GraphDataset',
+    'OnlineGraphDataset',
+    'QM9',
+    'DobsonDoigGraphDataset',
+    'EgoGraphDataset',
+    'SmallEgoGraphDataset',
+    'LobsterGraphDataset',
+    'PlanarGraphDataset',
+    'SBMGraphDataset',
+]
