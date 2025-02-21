910040389b3fa535c7853132358f5d6cb4f7d57e
Markus Krimmel <krimmel@biochem.mpg.de>
2025-02-21 11:02:25 +0100

    fix: renamed laplace_mmd to guassian_tv_mmd (#35)

--- graph_gen_gym/metrics/gran/__init__.py
+++ graph_gen_gym/metrics/gran/__init__.py
@@ -1,2 +1,2 @@
-from graph_gen_gym.metrics.gran.laplace_mmd import *  # noqa
+from graph_gen_gym.metrics.gran.gaussian_tv_mmd import *  # noqa
 from graph_gen_gym.metrics.gran.rbf_mmd import *  # noqa
