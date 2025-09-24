01189076c027f9732f3aca204e6463ef137643db
Markus Krimmel <krimmel@biochem.mpg.de>
2025-09-24 10:33:02 +0200

    Reformat print output

--- custom_metrics_demo.py
+++ custom_metrics_demo.py
@@ -8,6 +8,8 @@ from polygraph.utils.kernels import LinearKernel
 from polygraph.utils.descriptors import ClusteringHistogram
 from sklearn.linear_model import LogisticRegression
 
+logger.disable("polygraph")
+
 
 def betweenness_descriptor(graphs: Iterable[nx.Graph]) -> np.ndarray:
     """A custom graph descriptor that computes betweenness centrality.
@@ -34,10 +36,11 @@ def calculate_custom_mmd(
     This MMD uses a linear kernel based on betweenness centrality histograms.
     It is estimated using the unbiased minimum variance estimator.
     """
+    print("Calculating custom MMD...")
     mmd = DescriptorMMD2(
         reference, kernel=LinearKernel(betweenness_descriptor), variant="umve"
     )
-    logger.info(f"Computed Customized MMD: {mmd.compute(generated)}")
+    print(f"Custom MMD: {mmd.compute(generated)} \n")
 
 
 def calculate_custom_pgs(
@@ -51,6 +54,7 @@ def calculate_custom_pgs(
     PolyGraphScore may be instantiated with any descriptors implementing the `polygraph.utils.descriptors.GraphDescriptor` interface
     and any classifier implementing the `polygraph.metrics.base.polygraphscore.ClassifierProtocol` interface (i.e., the sklearn interface).
     """
+    print("Calculating custom PGS...")
     pgs = PolyGraphScore(
         reference,
         descriptors={
@@ -59,7 +63,14 @@ def calculate_custom_pgs(
         },
         classifier=LogisticRegression(),
     )
-    logger.info(f"Computed Customized PolyGraphScore: {pgs.compute(generated)}")
+    result = pgs.compute(generated)
+
+    print(f"Overall PGS: {result['polygraphscore']:.6f}")
+    print(f"Score Descriptor: {result['polygraphscore_descriptor']}")
+    print("\nSubscores:")
+    for metric, score in result["subscores"].items():
+        print(f"  {metric.capitalize()}: {score:.6f}")
+    print()
 
 
 if __name__ == "__main__":
@@ -67,4 +78,5 @@ if __name__ == "__main__":
     generated = list(SBMGraphDataset("val").to_nx())
 
     calculate_custom_mmd(reference, generated)
+    print("=" * 50, "\n")
     calculate_custom_pgs(reference, generated)
